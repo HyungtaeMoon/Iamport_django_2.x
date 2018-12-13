8d610893606b888afa8673188b6fbc5800cc99f0
HyungtaeMoon <blessmht@gmail.com>
2018-12-13 17:59:15 +0900

    [UPDATE] shop.models.Item 모델에 photo 필드 추가, pillow 설치

--- app/shop/models.py
+++ app/shop/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
     amount = models.PositiveIntegerField()
+    photo = models.ImageField()
     is_public = models.BooleanField(default=False, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
