91420105004db250882e92a38aed40c27acae988
HyungtaeMoon <blessmht@gmail.com>
2018-12-16 18:11:11 +0900

    [ADD] 상품 검색 시 queryset 보내도록 구현, ItemListView로 재정의

--- app/shop/views.py
+++ app/shop/views.py
@@ -8,3 +8,21 @@ index = ListView.as_view(model=Item,
                          queryset=Item.objects.filter(
                              is_public=True,
                          ))
+
+
+class ItemListView(ListView):
+    model = Item
+    queryset = Item.objects.filter(is_public=True)
+
+    def get_queryset(self):
+        self.q = self.request.GET.get('q', '')
+
+        qs = super().get_queryset()
+        if self.q:
+            qs = qs.filter(name__icontaines=self.q)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.q
+        return context
