b8d135395e0a1c2d3c86aeaa56c4fea1488603b8
HyungtaeMoon <blessmht@gmail.com>
2018-12-31 21:16:58 +0900

    [ADD] shop.admin.OrderAdmin 에 주문 갱신하도록 do_update 메서드 추가

--- app/shop/admin.py
+++ app/shop/admin.py
@@ -19,3 +19,16 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['imp_uid', 'user', 'name', 'amount_html', 'status_html', 'paid_at', 'receipt_link']
+    actions = ['do_update']
+
+    def do_update(self, request, queryset):
+        '주문 정보를 갱신합니다.'
+        total = queryset.count()
+        if total > 0:
+            for order in queryset:
+                order.update()
+            self.message_user(request, '주문 {}건의 정보를 갱신했습니다.'.format(total))
+        else:
+            self.message_user(request, '갱신할 주문이 없습니다')
+    # admin/shop/order 의 액션에서 Do_update 를 '선택된 주문들의 아임포트 갱신하기'로 보여주기
+    do_update.short_description = '선택된 주문들의 아임포트 갱신하기'
