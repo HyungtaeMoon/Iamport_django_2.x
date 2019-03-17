15dc408dcac817c700ffa0159421541bf67d34c7
HyungtaeMoon <blessmht@gmail.com>
2019-03-17 20:54:31 +0900

    [COMMENT] LoginView 주석 추가

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -7,6 +7,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    # CBV 로 LoginView 를 구성, Authentication 폼에서 username/password 의 인증을 수행
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     # path('login/', views.login_view, name='login'),
     path('profile/', views.profile, name='profile'),
